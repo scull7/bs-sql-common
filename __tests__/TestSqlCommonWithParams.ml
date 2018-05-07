11ee80639c91e95eaae42ab13724066ce5f4ba02
Aaron Barnett <aaron.barnett@outlook.com>
2018-05-07 14:12:05 -0700

    trimming down test file, time to recurse

--- __tests__/TestSqlCommonWithParams.ml
+++ __tests__/TestSqlCommonWithParams.ml
@@ -4,10 +4,6 @@ module Sql = SqlCommon.Make_sql(MySql2)
 
 external jsonIntMatrix : int array array -> Js.Json.t = "%identity"
 
-external jsonIntMatrixArr : int array array -> Js.Json.t = "%identity"
-
-external jsonStringMatrix : string array array -> Js.Json.t = "%identity"
-
 type result = {
   result: int;
 }
@@ -78,7 +74,7 @@ describe "Test parameter interpolation" (fun () ->
      id = json |> field "id" int;
      code = json |> field "code" string;
     }) in
-    let params = `Positional ( jsonIntMatrixArr [|[|1;2|]|]) in
+    let params = `Positional (jsonIntMatrix [|[|1;2|]|]) in
     let batch_size = 10 in
     Sql.query_batch conn ~batch_size ~sql:"SELECT * FROM test.simple WHERE test.simple.id IN (?)" ~params (fun res ->
     match res with
@@ -90,33 +86,4 @@ describe "Test parameter interpolation" (fun () ->
       |> finish
     )
   );
-
-
-
-  (* testAsync "Expect a batched INSERT with two positional parameters to succeed if batched" (fun finish ->
-    (* let decoder json = Json.Decode.({
-     id = json |> field "id" int;
-     code = json |> field "code" string;
-    }) in *)
-    (* let params = Some(`Positional ( jsonStringMatrix [|[|"cats";"dogs"|]|])) in *)
-    let batch_size = 10 in
-    let columns = [|"code"|] in
-    let rows = [|
-      ("pangolin");
-      ("2");
-    |] in
-    Sql.mutate_batch conn ~batch_size ~table:"simple" ~columns ~rows (fun res ->
-    match res with
-    | `Error e -> let _ = Js.log e in finish (fail "see log")
-    | `Mutation (rows, b) ->
-      let _ = Js.log(rows) in
-      let _ = Js.log(b) in
-      [|1;2|]
-      |> Expect.expect
-      |> Expect.toHaveLength 2
-      |> finish
-    )
-  ); *)
-
-
 );
