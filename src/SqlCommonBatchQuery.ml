8fe4de8e3df14a9cac99354fbae3ed9ae99861e4
Aaron Barnett <aaron.barnett@outlook.com>
2018-05-07 14:05:25 -0700

    removing some unused logs

--- src/SqlCommonBatchQuery.ml
+++ src/SqlCommonBatchQuery.ml
@@ -1,11 +1,8 @@
-(* external sqlformatparams : string -> [`Positional of Js.Json.t] -> string = "format" *)
-(* [@@bs.module "sqlstring"] *)
-external sqlformatparams : string -> Js.Json.t -> string = "format"
+external sqlformat : string -> Js.Json.t -> string = "format"
 [@@bs.module "sqlstring"]
 
 external array_to_params : 'a array -> [`Positional of Js.Json.t] = "%identity"
 external params_to_array : [`Positional of Js.Json.t] -> 'a array = "%identity"
-external params_to_json : [`Positional of Js.Json.t] -> Js.Json.t = "%identity"
 
 type iteration = {
   params: [`Positional of Js.Json.t];
@@ -23,30 +20,21 @@ let iteration ?prev params data meta  =
 
 (* Lowest *)
 let db_call ~execute ~sql ?params ~fail ~ok_db _ =
-  let _ = Js.log("sql", sql) in
   let _ = execute ~sql ?params (fun res ->
-    let _ = Js.log("res", res) in
     match res with
     | `Error e -> fail e
     | `Select ((data:Js.Json.t array), (meta:MySql2.meta)) -> ok_db data meta
   )
   in ()
 
-let unwrap_params params =
+(* Takes the params from positional to 'a for sqlformat *)
+let unwrap params =
   match params with
   | `Positional p -> p
 
-
 (* Does substitution, calls db *)
 let query_batch ~execute ~sql ~params ~fail ~ok_db _ =
-  (* let _ = Js.log("params in query_batch", params) in *)
-  let usable_params = unwrap_params params in
-  (* let _ = Js.log("a", a) in *)
-  (* let pa = match params with
-  | `Positional p -> Js.log("some p", p)
-  in *)
-  (* let _ = Js.log("pa", pa) in *)
-  let sql_with_params = sqlformatparams sql (unwrap_params params) in
+  let sql_with_params = sqlformat sql (unwrap params) in
   db_call ~execute ~sql:sql_with_params ~fail ~ok_db ()
 
 let iterate ~query_batch_partial ~batch_size ~(params:[`Positional of Js.Json.t]) ~fail ~ok_iteration ~prev _ =
@@ -95,14 +83,6 @@ let rec run ~batch_size ~iterator ~fail ~ok_db ~iteration =
   query_batch ~fail ~ok () *)
 
 let query execute ?batch_size ~sql ~params user_cb =
-  let _ = Js.log("params in query", params) in
-  (* let _ = match params with
-  | `Positional p -> Js.log("some p", p)
-  | _ -> Js.log("NOOOO") *)
-  (* in *)
-  let params_matched = match params with
-  | `Positional p -> Js.log("some p", p)
-  in
   let batch_size =
     match batch_size with
     | None -> 1000
